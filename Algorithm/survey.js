cd8c21e99c360ef01d3feefb6ce25214ca2fa69e
rahe01 <rahatrahe01@gmail.com>
2025-11-02 00:44:56 +0600

    survay

---
+++ Algorithm/survey.js
@@ -0,0 +1,32 @@
+//* Grouping and Aggregating Data
+
+// Scenario: Count every survey and group by response
+
+//? input
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+//? Output
+// { A: 5, C: 3, B: 5, D: 1 }
+
+
+const count = surveyResponses.reduce((acc, response) => {
+  acc[response] = (acc[response] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
