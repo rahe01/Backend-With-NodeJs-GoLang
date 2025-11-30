576329bd69cedc237460f47b0b99a898afb66aa7
rahe01 <rahatrahe01@gmail.com>
2025-11-30 17:37:19 +0600

    express basic crud

--- ExpressJS/CURD With PostgreSQL/src/server.ts
+++ ExpressJS/CURD With PostgreSQL/src/server.ts
@@ -1,28 +1,26 @@
-import express, { Request, Response } from "express"
-import {Pool} from "pg";
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
 
-
-dotenv.config({path: path.join(process.cwd() , '.env')});
-
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const app = express();
 const port = 5000;
 
 // parser
+
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded({ extended: true }));
 
 
 // DB
 const pool = new Pool({
-    connectionString: `${process.env.CONNECTION_STR}`
+  connectionString: `${process.env.CONNECTION_STR}`,
 });
 
-
-const initDB = async() =>{
-    await pool.query(`  
+const initDB = async () => {
+  await pool.query(`  
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(200) NOT NULL,
@@ -33,9 +31,9 @@ const initDB = async() =>{
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()   
         )
-        `)
+        `);
 
-    await pool.query(`
+  await pool.query(`
         CREATE TABLE IF NOT EXISTS todos(
         id SERIAL PRIMARY KEY,
         user_id INT REFERENCES users(id) ON DELETE CASCADE,
@@ -44,34 +42,244 @@ const initDB = async() =>{
         completed BOOLEAN DEFAULT false,
         due_date DATE,
         created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW() 
-
-        
-        
-        
+        updated_at TIMESTAMP DEFAULT NOW()     
         )
         
         
-        `)
+        `);
+};
+initDB();
+
+
+
+
+// logger middleware 
+
+const logger : any= (req:Request, res: Response, next: NewableFunction) =>{
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+    next();
 }
-initDB()
 
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World! Next level')
-})
 
 
-app.post("/" , (req: Request , res: Response) =>{
-    console.log(req.body);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello World! Next level");
+});
+
+// ======================================== Users CRUD ============================================
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email) VALUES($1 , $2) RETURNING *`,
+      [name, email]
+    );
+
+    // console.log(result.rows[0]);
+
+    // res.send({message: "data inserted"})
+    res.status(201).json({
+      success: true,
+      message: "Data inserted",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// get all users
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
     res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+// get single user
+app.get("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+
+  // res.send({message : "Api is coll"})
+
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
         success: true,
-        message: "API is work"
+        message: "user  found",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// update user
+app.put("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+
+  // res.send({message : "Api is coll"})
+
+  const {name, email} = req.body;
+
+  try {
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *` , [name, email, req.params.id]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "user updated",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// delet user
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `DELETE FROM users WHERE id = $1`,
+      [req.params.id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+// ======================================== Todos CRUD ===========================================
+
+
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  console.log("REQ.BODY:", req.body);  // <-- add this
+
+  if (!user_id || !title) {
+    return res.status(400).json({
+      success: false,
+      message: "user_id and title are required",
+    });
+  }
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Todo created",
+      data: result.rows[0],
+    });
+
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+// ===================================Not found=========================================
+
+app.use((req:Request, res: Response) =>{
+
+    res.status(404).json({
+        success:false,
+        message: "Route not found",
+        path: req.path
     })
+
+
 })
+
+
+
+
+
+
+
+
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
