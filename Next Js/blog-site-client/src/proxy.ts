9d43e0f2797aecde4d26cdda6037c6a7d043a315
rahe01 <rahatrahe01@gmail.com>
2026-01-19 22:01:00 +0600

    blog fetch

--- Next Js/blog-site-client/src/proxy.ts
+++ Next Js/blog-site-client/src/proxy.ts
@@ -1,15 +1,44 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constants/role";
 
- 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
-    console.log("Hello ",request.url);
+  const pathName = request.nextUrl.pathname;
+
+  let isAuthenticated = false;
+  let isAdmin = false;
+
+  const { data } = await userService.getSession();
+
+  if (data) {
+    isAuthenticated = true;
+    isAdmin = data.user.role === Roles.admin;
+  }
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isAdmin && pathName.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+
+  if (!isAdmin && pathName.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
   return NextResponse.next();
 }
- 
+
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
-  matcher: ["/dashboard"],
-}
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/admin-dashboard",
+    "/admin-dashboard/:path*",
+  ],
+};
